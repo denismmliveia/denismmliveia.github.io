69bdcab5a37f74c96909db33bd399130b0add490
Denis <denis@rave.local>
2026-04-09 16:24:57 +0200

    feat(functions): export revokeLink, expireLinks, blockUser, reportUser + deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,7 +9,13 @@ export { generateQrToken, validateQrToken } from './qr/generateQrToken';
 // Link
 export { initiateLink } from './link/initiateLink';
 export { confirmLink } from './link/confirmLink';
+export { expireLinks } from './link/expireLinks';
+export { revokeLink } from './link/revokeLink';
 
 // Chat
 export { requestPhotoUploadUrl } from './chat/requestPhotoUploadUrl';
 export { getPhotoViewUrl } from './chat/getPhotoViewUrl';
+
+// Moderation
+export { blockUser } from './moderation/blockUser';
+export { reportUser } from './moderation/reportUser';
